34653439b884b58d67055c220f9fbefd43dc8ab6
jq6914 <jq6914@naver.com>
2025-05-15 21:06:50 +0900

    div,css 배웠다다

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<div id="a">
+<h1 id='b'>여행 추천 프로그램</h1>
+
+</div>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    #a {
+        background-color: red;
+        width: 100%;
+    }
+    #b {
+        width: 10px;
+    }
+</style>
