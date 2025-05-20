045924bf40fd1e97b71c9c08cf59aff1bae6edf0
ThefastestmaninChangwon <atopbbb@gmail.com>
2025-05-21 01:20:54 +0900

    나혼자 하려다 제대로 깨진날날

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,37 @@
-<div id="a">
-<h1 id='b'>려유</h1>
+<div>
+    <div id="top_bar">
+        <h1 id='name'>려유</h1>
+        <div>
+            <div id = top_bar_right>
+                <input >
+                <div>
+                    <button>
+                       로그인 
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <style>
-    #a {
-        background-color: red;
+    #top_bar{
         width: 100%;
+        flex-direction: column;
+        background-origin:line;
+        border-bottom: 3px solid green;
+        display: flex;  
+        justify-content: space-between;
     }
-    #b {
-        width: 10px;
+    #name {
+        width: 100px;
+        display: flex;
+        flex-direction: column;
+    }
+    #top_bar_right{
+        display: flex;
     }
 </style>
