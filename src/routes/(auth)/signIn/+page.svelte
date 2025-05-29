5229ba0c3afdf6b64a4e83cda091940f7300f5bd
jq6914 <jq6914@naver.com>
2025-05-29 19:21:44 +0900

    rogin 1차차

---
+++ src/routes/(auth)/signIn/+page.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+    import {supabase} from "$lib/supabaseClient";
+
+    let email: string
+    let pw: string
+
+    const signInBtn = async () => {
+        const { data: signInData, error: signInError } = await supabase
+            .from("rogin")
+            .upsert({ email: email, password: pw })
+            .select()
+
+        if (signInError) return console.error(signInError)
+
+        if (signInData) return console.log(signInData)
+    }
+    
+</script>
+
+<div id="main_box">
+    <div id="top_bar">
+        <h1 id="title_rogo">
+            <a href="/" id="title_a">AI:ON</a>
+        </h1>
+    </div>
+    <div id="body">
+        <div id="login_box">
+            <h1 id="login_text">로그인</h1>
+            <p id="welcome_text">"려유"의 세게에 오신것을 환영합니다.</p>
+            <input placeholder="이메일 주소" class="login_input" bind:value={email} />
+            <input placeholder="비밀번호" class="login_input" bind:value={pw} type="password" />
+            <button id="login_btn" on:click={signInBtn}>로그인</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    #main_box {
+        width: 100%;
+        height: 100%;
+    }
+
+    #top_bar {
+        width: 100%;
+        height: 100px;
+    }
+
+    #title_rogo {
+        margin-left: 15px;
+    }
+
+    #title_a {
+        text-decoration-line: none;
+        color: black;
+    }
+
+    #body {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    #login_box {
+        width: 460px;
+        height: 737px;
+        border: 3px solid gray;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #login_text {
+        width: 100%;
+        text-align: center;
+    }
+
+    #welcome_text {
+        width: 100%;
+        text-align: center;
+    }
+
+    .login_input {
+        width: 310px;
+        height: 24px;
+        padding: 12px 16px;
+        border: 2px solid gray;
+        border-radius: 10px;
+        margin: 5px 0;
+    }
+
+    #login_btn {
+        width: 345px;
+        height: 50px;
+        padding: 14px 18px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid gray;
+        border-radius: 10px;
+        margin: 10px 0;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,110 +1,119 @@
-<script lang='ts'>
-    let menuToggle:boolean = false
+<script lang="ts">
+    let menuToggle: boolean = false
     let searchToggle: boolean = false
+    let travelMenuTooggle: boolean = false
 
     const MenuSwitch = () => {
         menuToggle = !menuToggle
     }
 
-    const searchSwitch = () => {
+    const SearchSwitch = () => {
         searchToggle = !searchToggle
+    }
 
+    const TravelMenuSwitch = () => {
+        travelMenuTooggle = !travelMenuTooggle
     }
 </script>
 
 {#if searchToggle}
-    <div id='search_body'>
-        <div id='search_bg'>
-        </div>
-        <div id='search_bar'>
-            <div id='search_top_bar'>
-                <h1 id='search_title'>려유</h1>
-                <div id='search_bar_search_box'>
-                    <button class='top_bar_btn'>
-                        <img src="/icon/search.svg" alt='돋보기 사진'/>
+    <div id="search_body">
+        <div id="search_bg"></div>
+        <div id="search_bar">
+            <div id="search_top_bar">
+                <h1 id="search_title">려유</h1>
+                <div id="search_bar_box">
+                    <button class="top_bar_btn">
+                        <img src="icon/search.svg" alt="돋보기 사진"/>
                     </button>
-                    <input id='search_bar_search'/>
+                    <input id="search_bar_search"/>
                 </div>
-                <button on:click={searchSwitch} class='close_btn'>
-                    <img src='icon/x.svg' alt='X 이미지' class='close_img'>
+                <button on:click={SearchSwitch} class="close_btn">
+                    <img src="icon/x.svg" alt="X 이미지" class="close_img"/>
                 </button>
             </div>
-            <div id='search_bar_body'>
-                <div id='popular_box'>
+            <div id="search_bar_body">
+                <div id="popular_box">
                     <h2>인기 검색어</h2>
                     <div>
-                        <button class='popular'>제주</button>
-                        <button class='popular'>부산</button>
-                        <button class='popular'>경주</button>
-                        <button class='popular'>창원</button>
+                        <button class="popular">제주</button>
+                        <button class="popular">부산</button>
+                        <button class="popular">경주</button>
+                        <button class="popular">창원</button>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
 {/if}
 
 {#if menuToggle}
-    <div id='menu_body'>
-        <div id='menu_bg'>
+    <div id="menu_body">
+        <div id="menu_bg">
 
         </div>
-        <div id='menu_bar'>
-            <div id='close_bg'>
-                <button on:click={MenuSwitch} class='close_btn'>
-                    <img src='icon/x.svg' alt='X 이미지' id='close_img'>
+        <div id="menu_bar">
+            <div id="close_bg">
+                <button on:click={MenuSwitch} class="close_btn">
+                    <img src="icon/x.svg" alt="X 이미지" class="close_img"/>
                 </button>
             </div>
 
-            <div class='menu_txt_bg'>
-                <h1>
-                    <a href='/signIn' class='menu_txt'> 로그인 </a>
+            <div class="menu_txt_backgroun">
+                <h1 class="sign_txt">
+                    <a href="/signIn" class="sign">로그인</a>
                     /
-                    <a href='/signUp' class='menu_txt'> 회원가입 </a>
+                      <a href="/signUp" class="sign">회원가입</a>
                 </h1>
             </div>
 
-            <div class='menu_txt_bg'>
-                <h2>
-                    <a href='/' class='menu_txt'> 교통편 </a>
+            <div class="menu_txt_bg">
+                <h2 class="menu_txt_title">
+                    <a href="/" class="menu_txt">교통편</a>
                 </h2>
             </div>
-
-            <div class='menu_txt_bg'>
-                <h2>
-                    <a href='/' class='menu_txt'> 도시별 여행 정보 </a>
+            <div class="menu_txt_bg">
+                <h2 class="menu_txt_title">
+                    <a href="/" class="menu_txt">도시별 여행정보</a>
+                    <span class = 'free'> 무료로 확인해 보세요! </span>
                 </h2>
             </div>
-            
-            <div class='menu_txt_bg'>
-                <h2>
-                    <a href='/' class='menu_txt'> 렌터카 </a>
+            <div class="menu_txt_bg">
+                <h2 class="menu_txt_title">
+                    <a href="/" class="menu_txt">렌터카</a>
                 </h2>
             </div>
-
-            <div class='menu_txt_bg'>
-                <h2>
-                    <a href='/' class='menu_txt'> 숙소 </a>
-                </h2>
+            <div class="menu_txt_bg">
+                <button on:click={TravelMenuSwitch} id="menu_menu_btn">
+                    <h2 class="menu_txt_title">
+                        <a href="/" class="menu_txt">여행 상품</a>
+                        {travelMenuTooggle ? '▲' : '▼'}
+                    </h2>
+                </button>
+                {#if travelMenuTooggle}
+                    <ul id="menu_menu_list">
+                        <li>항공</li>
+                        <li>숙소</li>
+                        <li>투어 & 티켓켓</li>
+                    </ul>
+                {/if}
             </div>
         </div>
     </div>
 {/if}
 
-<div>
-    <div id='tor_bar'>
-        <h1 id='title_text'>려유</h1>
-        <div id='top_bar_right'>
-            <div id='search_box'>
-                <button class='top_bar_btn'>
-                    <img src="/icon/search.svg" alt='돋보기 사진'/>
+<div id="main_bg">
+    <div id="tor_bar">
+        <h1 id="title_text">려유</h1>
+        <div id="top_bor_right">
+            <div id="search_box">
+                <button class="top_bar_btn">
+                    <img src="icon/search.svg" alt="돋보기 사진"/>
                 </button>
-                <input id='search' on:click={searchSwitch}/>
-
+                <input id="search" on:click={SearchSwitch}/>
             </div>
-            <button class='top_bar_btn' on:click={MenuSwitch}>
-                <img src="/icon/menu-2.svg" alt='메뉴 사진' id='menu_btn'/>
+            <button class="top_bar_btn" on:click={MenuSwitch}>
+                <img src="icon/menu-2.svg" alt="메뉴 사진" id="menu_btn"/>
             </button>
         </div>
     </div>
@@ -113,18 +122,20 @@
 <style>
     #search_body {
         width: 100%;
-        position: absolute;
         height: 100%;
+        position: absolute;
     }
+
     #search_bg {
         width: 100%;
         height: 100%;
         position: absolute;
         background-color: black;
-        opacity: 0.6;
+        opacity : 0.5;
         display: flex;
         justify-content: end;
     }
+
     #search_bar {
         width: 100%;
         height: 40%;
@@ -132,154 +143,211 @@
         top: 0;
         background-color: white;
     }
-    #search_top_bar{
+
+    #search_top_bar {
         width: 100%;
         display: flex;
         align-items: center;
         justify-content: space-between;
-        
     }
-    #search_bar_search_box{
-        display: flex;
-        background-color: #f2f2f7;
-        border-radius: 30px;
-        padding: 10px;
-        
-    }
+
     #search_title {
         margin-left: 20px;
     }
-    #search_bar_search {
-        width: 800px;
-        height: 20px;
-        border: none;
+
+    #search_bar_box {
+        display: flex;
+        background-color: #f3f3f3;
+        border-radius: 30px;
         padding: 7px;
-        background-color: #f2f2f7;
-        border-radius: 20px;
     }
+
+    #search_bar_search {
+        width: 750px;
+        height: 30px;
+        border: none;
+        padding: 3px 8px;
+        background-color: #f3f3f3;
+        border-radius: 30px;
+    }
+
     #search_bar_body {
         width: 100%;
         display: flex;
         justify-content: center;
-
     }
 
     #popular_box {
         width: 800px;
-        
     }
 
     .popular {
-        font-size: 20px;
+        font-size: 18px;
         border: none;
-        border-radius: 20px;
+        border-radius: 25px;
         padding: 5px 20px;
         margin-right: 20px;
         font-weight: 400;
-
     }
 
+    #search_bar_search:focus {
+        outline: none;
+    }
 
     #menu_body {
         width: 100%;
         height: 100%;
         position: absolute;
     }
+
     #menu_bg {
         width: 100%;
         height: 100%;
         position: absolute;
         background-color: black;
-        opacity: 0.6;
+        opacity : 0.5;
         display: flex;
         justify-content: end;
     }
+
     #menu_bar {
         width: 30%;
         height: 100%;
         background-color: white;
         position: absolute;
-        right: 0%;
-
+        right: 0;
+        top: 0;
+        padding: 20;
+        box-shadow: -2px 0 10px rbga(0,0,0,0.2);
+        overflow-y: outo;
     }
+
     #close_bg {
         width: 100%;
-        background-color: #3e74fe;
         padding: 15px;
+        background-color: white;
+        height: 100px;
     }
+
     .close_btn {
         background-color: transparent;
         border: none;
+        margin-right: 20px;
         cursor: pointer;
+        margin-left: 15px;
     }
+
     .close_img {
         width: 35px;
         height: 35px;
     }
+    
+    .free {
+        background-color: #6090fe;
+        color: white;
+        padding: 3px 8px;
+        border-radius: 15px;
+        font-size: 12px;
+        margin-left: 10px;
+        align-items: center;
+    }
+
     .menu_txt_bg {
         width: 100%;
-        background-color: white;
-        height: 50px;
+        height: auto;
         display: flex;
-        align-items: center;
-        border-bottom: 2px solid black;
-        padding: 10px 20px
-
+        justify-content: center;
+        border-bottom: 1px solid black;
+        flex-direction: column;
+        padding: 10px 30px
     }
+
+    #menu_menu_btn {
+        background-color: transparent;
+        align-items: center;
+        cursor: pointer;
+        border: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    #menu_menu_list {
+        margin-left: 10px;
+    }
+
+    .menu_txt_title {
+        margin: 0 20px 0 20px;
+        margin-right: 100px;
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.5rem;
+    }
+
     .menu_txt {
         color: black;
         text-decoration-line: none;
         font-family: 'Noto Sans KR', sans-serif;
-
     }
+
     #tor_bar {
+        border-bottom: 2px solid #6090fe;
         background-color: white;
-        border-bottom: 2px solid #3e74fe;
         padding: 20px;
         display: flex;
         align-items: center;
         justify-content: space-between;
     }
+
     #title_text {
         margin: 0;
         font-family: 'Noto Sans KR', sans-serif;
     }
-    #top_bar_right {
+
+    #top_bor_right {
         display: flex;
     }
-    .top_bar_btn {
-        background-color: transparent;
-        border: none;
-        display: flex;
-        align-items: center;
-        margin-right: 10px;
-        cursor: pointer;
-    }
+
     #search_box {
         display: flex;
         align-items: center;
-        border: 3px solid #3e74fe;
+        border: 3.5px solid #85a7ff;
         padding: 5px;
         height: 30px;
-        border-radius: 20px;
-        margin-right: 30px;
-
+        border-radius: 25px;
+        margin-right: 10px;
     }
 
     #search {
-        border: none;
-        background-color: transparent;
         width: 200px;
         height: 25px;
+        border: none;
     }
 
     #search:focus {
         outline: none;
     }
 
-    #menu_btn {
-        width: 35;
-        height: 35;
+    .top_bar_btn {
+        background-color: transparent;
+        border: none;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
     }
 
+    #menu_btn {
+        width: 35px;
+        height: 35px;
+    }
+    .sign {
+        color: black;
+        text-decoration: none;
+    }
+   .sign_txt {
+        margin-bottom: 20px;
+        padding-bottom: 10px;
+        color: black;
+        text-decoration: none;
+        margin-left: 40px;
+    }
 </style>
