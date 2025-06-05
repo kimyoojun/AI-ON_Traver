6f1fa29ee450a824acec340c2d30f20761355a55
lhm2005-l <lhyunmin1108@gmail.com>
2025-06-06 00:37:28 +0900

    회원가입 시 필요한 부가적인 정보들 슈파베이스와 동기화

--- src/routes/(auth)/sign_up/+page.svelte
+++ src/routes/(auth)/sign_up/+page.svelte
@@ -1,19 +1,50 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import { session } from "$lib/store/session";
     import { supabase } from "../../../lib/supabaseClient";
     import { uuid } from "@supabase/supabase-js/dist/main/lib/helpers";
 
     let email: string
     let pw: string
+    let name: string
+    let age: string
+    let number: number  // 전화번호를 닫는 변수 
+    let user_id: string // user의 uuid를 담음 
+
+    let page = {
+        a: 1,
+        b: 2,
+    }
 
     const sign_up_btn = async () => {
-        const {error} = await supabase.auth.signUp({
+        if (page.a) {
+            const {error, data} = await supabase.auth.signUp
+            ({
             email: email,
             password: pw
-        })
-        if (error) return console.error(error)
+            })
+        
+            if (error) return console.error(error)
+
+            user_id = data?.user?.id ?? ""     // ?는 값이 없을수도 있다는것을 의미 
+
+            page.a += 1                     // 이상
+        
+        } else if (page.b) {
+            const { error: usersError, data: usersData} = await supabase
+                .from('users')
+                .insert({
+                    id: user_id,
+                    age: age,
+                    numger: number,
+                    name: name
+                })
+
+            if (usersError) return console.error(usersError)
+        }
+
         await goto("/sign_in")
-    }
+        }
 </script>
 
 <div id="main_box">
@@ -24,11 +55,25 @@
     </div>
     <div id="body">
         <div id="login_box">
-            <h1 id="login_text">회원가입</h1>
-            <p id="welcome_text">"려유"를 찾아 오신것을 환영합니다.</p>
-            <input placeholder="이메일 주소" class="login_input" bind:value={email}/>
-            <input placeholder="비밀번호" class="login_input" bind:value={pw} type="password"/>
-            <button id="login_btn" on:click={sign_up_btn}>회원가입</button>
+            {#if page.a}
+                <h1 id="login_text">회원가입</h1>
+                <p id="welcome_text">"려유"를 찾아 오신것을 환영합니다.</p>
+                <input placeholder="이메일 주소" class="login_input" bind:value={email}/>
+                <input placeholder="비밀번호" class="login_input" bind:value={pw} type="password"/>
+            {:else if page.b}
+                <h1 id="login_text">회원가입</h1>
+                <p id="welcome_text">"려유"를 찾아 오신것을 환영합니다.</p>
+                <input placeholder="이름" class="login_input" bind:value={name}/>
+                <input placeholder="전화번호" class="login_input" bind:value={number}/>
+                <input placeholder="나이" class="login_input" bind:value={age}/>
+            {/if}
+            <button id="login_btn" on:click={sign_up_btn}>
+                {#if page.a}
+                    다음
+                {:else if page.b}
+                    회원가입
+                {/if}
+            </button>
         </div>
     </div>
 </div>
